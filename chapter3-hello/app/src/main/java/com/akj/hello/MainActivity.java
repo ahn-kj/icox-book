e0a0d458c950a3d94b4c1016bebd31ef5a4e0ccc
akj61300 <rnlwjd0613@gmail.com>
2018-07-19 02:42:48 +0900

    3.8. if-else for while 실습

--- chapter3-hello/app/src/main/java/com/akj/hello/MainActivity.java
+++ chapter3-hello/app/src/main/java/com/akj/hello/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * MainActivity 가 최초 실행될 때 실행된다.
      * 자바독 주석을 확인하려면 F1 키를 누르자.
+     *
      * @param savedInstanceState
      */
     @Override
@@ -31,21 +32,30 @@ public class MainActivity extends AppCompatActivity {
                 // 클릭 카운트를 1회 증가시킨다.
                 clickCount = clickCount + 1;
 
-                // 버튼이 클릭되면 클릭된 횟수를 증가하면서 토스트 메세지를 보여준다.
-                Toast.makeText(getApplicationContext(), "clickCount: " + clickCount, Toast.LENGTH_SHORT).show();
+                // if - else 문 실습
+                /*
+                if (clickCount % 2 == 0) {
+                    // 클릭 카운트가 2의 배수이면 버튼이 클릭된 횟수를 잠시 보여준다.
+                    Toast.makeText(getApplicationContext(), "clickCount: " + clickCount, Toast.LENGTH_SHORT).show();
+                } else if (clickCount % 3 == 0) {
+                    // 클릭 카운트가 3의 배수이면 Hello, World 메세지를 보여준다.
+                    Toast.makeText(getApplicationContext(), "Hello, world!", Toast.LENGTH_SHORT).show();
+                } else {
+                    //  클릭 카운트가 2의 배수도 아니고 3의 배수도 아닌 경우 "Hello" 메세지를 잠시 보여준다.
+                    Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
+                }
+                */
+
+                // 클릭된 횟수만큼 반복하면서 반복된 횟수 메세지를 보여준다.
+                for (int i = 0; i < clickCount; i++) {
+                    // 만일 반복횟수가 3번을 넘어선다면 반복문을 중지한다.
+
+                    if(i >= 3){
+                        break;
+                    }
+                    Toast.makeText(getApplicationContext(), "반복: " + (i + 1), Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
-        // 변수 var1 을 선언한다.
-        int var1 = 0;
-        {
-            // 블록내에서도 변수 var1 을 접근 가능하다.
-            var1 = 2;
-
-            int var2 = 0;
-        }
-
-        // 블록내에서 선언된 변수 var2 는 블록 밖에서 접근할 수 없다.
-        // var2 = 1
     }
 }
